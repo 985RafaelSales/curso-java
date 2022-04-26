3bddfc8298ba2c6b1418c15ca823be97bdcc27fe
985RafaelSales <104109240+985RafaelSales@users.noreply.github.com>
2022-04-25 21:58:22 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;//não esqueça de colocar o import em cima do class.
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner (System.in);
+		int n1=0; //não esqueça declarar variavel.
+		System.out.println("Qual sua idade");
+		n1 = sc.nextInt();
+	}
+}
